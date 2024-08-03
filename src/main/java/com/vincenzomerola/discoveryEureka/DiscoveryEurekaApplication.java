8447852dcb4eff60109094be5effae61e65e73b2
V-Merola <vincenzo.merola18@gmail.com>
2024-08-03 16:38:59 +0200

    Inizializzazione Progetto

---
+++ src/main/java/com/vincenzomerola/discoveryEureka/DiscoveryEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.vincenzomerola.discoveryEureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryEurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vincenzomerola/discoveryEureka/DiscoveryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincenzomerola.discoveryEureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
