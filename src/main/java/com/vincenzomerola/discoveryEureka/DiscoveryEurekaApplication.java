f8229087f299ce2d4ab0a498de2e95a414c980ba
V-Merola <vincenzo.merola18@gmail.com>
2024-08-03 18:09:19 +0200

    Configurato Eureka Server

--- src/main/java/com/vincenzomerola/discoveryEureka/DiscoveryEurekaApplication.java
+++ src/main/java/com/vincenzomerola/discoveryEureka/DiscoveryEurekaApplication.java
@@ -2,8 +2,10 @@ package com.vincenzomerola.discoveryEureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryEurekaApplication {
 
 	public static void main(String[] args) {
